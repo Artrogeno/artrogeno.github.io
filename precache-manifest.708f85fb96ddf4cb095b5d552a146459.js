7dfa26b0c62040ea482bc97087f02b4f3d14ebed
Silva, Arthur Costa d <arthur.costa.d.silva@accenture.com>
2019-05-28 17:34:30 -0300

    feat: v0.1

---
+++ precache-manifest.708f85fb96ddf4cb095b5d552a146459.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c02d91208b4696f6fe80c0f1717b70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b4f1887517e56fdb558",
+    "url": "/static/css/2.34e6fd40.chunk.css"
+  },
+  {
+    "revision": "9f3cbf381821d6f033b8",
+    "url": "/static/css/main.786594ae.chunk.css"
+  },
+  {
+    "revision": "2b4f1887517e56fdb558",
+    "url": "/static/js/2.0ad3ffe9.chunk.js"
+  },
+  {
+    "revision": "9f3cbf381821d6f033b8",
+    "url": "/static/js/main.bb9620e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2f73cc73bdfb2fa508cac5ba9440ddcc",
+    "url": "/static/media/artrogenos.2f73cc73.svg"
+  },
+  {
+    "revision": "31cea6e9c3753b299914f1b1c5bddb0b",
+    "url": "/static/media/artrogenos.31cea6e9.woff"
+  },
+  {
+    "revision": "7318a2d58a19067a5c3963f63448deae",
+    "url": "/static/media/artrogenos.7318a2d5.eot"
+  },
+  {
+    "revision": "d598bc2ba24240711c615e45eccb7825",
+    "url": "/static/media/artrogenos.d598bc2b.ttf"
+  }
+]);
